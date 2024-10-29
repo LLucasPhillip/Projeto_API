b3d033c1d59285fb48c5834c8bcec1e1ee893419
Lucas <lucasp.panta@gmail.com>
2024-10-29 17:39:24 -0300

    Atualizando estrutura do projeto novamente

--- API-s-Python/CountWithTest/count.py
+++
@@ -1,9 +0,0 @@
-def contador(texto):
-    palavras = texto.split()
-    dici = {}
-    for palavra in palavras:
-        if palavra not in dici:
-            dici[palavra] = 1
-        else:
-            dici[palavra] += 1
-    return dici
